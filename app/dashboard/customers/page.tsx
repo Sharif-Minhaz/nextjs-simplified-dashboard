39684be0388c53115151b99f431163dbf3aee94e
Minhaz <sharif35-3001@diu.edu.bd>
2023-12-29 16:24:34 +0600

    feat: customer page added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function CustomersPage() {
-  return <div>CustomersPage</div>;
+import Table from '@/app/ui/customers/table';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
+export default async function CustomersPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  return (
+    <div className="w-full">
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <Table />
+      </Suspense>
+    </div>
+  );
 }
