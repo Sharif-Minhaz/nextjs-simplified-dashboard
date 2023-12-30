b6082f4ff079e662b1c723851a750a05a43286e9
Minhaz <sharif35-3001@diu.edu.bd>
2023-12-30 14:50:42 +0600

    feat: metadata added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,5 @@
 import Table from '@/app/ui/customers/table';
+// import Pagination from '@/app/ui/invoices/pagination';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 
@@ -15,6 +16,9 @@ export default async function CustomersPage({
       <Suspense fallback={<InvoicesTableSkeleton />}>
         <Table />
       </Suspense>
+      {/* <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={6} />
+      </div> */}
     </div>
   );
 }
